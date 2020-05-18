b2341feb73baa8fe510762d4ebfd22daa929a403
Mina91 <marina.inokuchi@stud.hn.de>
2020-05-18 22:50:00 +0200

    update

---
+++ exception.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Mina on 18.05.2020.
+//
+
+#ifndef UMGEKEHRTER_POLE_CPP_EXCEPTION_HPP
+#define UMGEKEHRTER_POLE_CPP_EXCEPTION_HPP
+
+#include <exception>
+
+class InvalidSyntaxException : public std::exception {};
+
+class InvalidNumberException : public std::exception {};
+
+class TooFewOperandsException : public std::exception{};
+
+#endif //UMGEKEHRTER_POLE_CPP_EXCEPTION_HPP
--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
 #include <iostream>
+#include "stack.hpp"
+#include <vector>
 
 int main() {
-
+    //using namespace std;
+    //vector<int> values;
+    //for (int i = 1; i <= 5; i++)
+    //    values.push_back(i);
+    //
+    //cout << *values.begin();
+    //cout << *values.begin();
+    //for (auto i = values.begin(); i != values.end(); ++i)
+    //    cout << *i << endl;
+    //cout << "*values.begin(): " << *values.begin() << endl;
+    //cout << "*++values.begin(): " << *++values.begin() << endl;
+    //cout << "*--values.end(): " << *--values.end() << endl;
+    //cout << "*----values.end(): " << *----values.end() << endl;
+    //cout << endl;
+    //
+    //cout << "*values.cbegin(): " << *values.cbegin() << endl;
+    //cout << "*++values.cbegin(): " << *++values.cbegin() << endl;
+    //cout << "*--values.cend(): " << *--values.cend() << endl;
+    //cout << "*----values.cend(): " << *----values.cend() << endl;
+    //cout << endl;
+    //
+    //cout << "*values.rbegin(): " << *values.rbegin() << endl;
+    //cout << "*++values.rbegin(): " << *++values.rbegin() << endl;
+    //cout << "*--values.rend(): " << *--values.rend() << endl;
+    //cout << "*----values.rend(): " << *----values.rend() << endl;
+    //cout << endl;
+    //
+    //cout << "*values.crbegin(): " << *values.crbegin() << endl;
+    //cout << "*++values.crbegin(): " << *++values.crbegin() << endl;
+    //cout << "*--values.crend(): " << *--values.crend() << endl;
+    //cout << "*----values.crend(): " << *----values.crend() << endl;
+    //cout << endl;
+    //
+    //cout << "values.front(): " << values.front() << endl;
+    //cout << "values.back(): " << values.back() << endl;
+    //values.pop_back();
+    //cout << "values.back(): " << values.back() << endl;
+    
 }
